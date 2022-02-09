1ceeabe7e4d80da868dcd39c7bd52f7175b71d85
Bruno <brunostel@gmail.com>
2022-02-08 21:23:32 -0300

    feat: add home elements

--- src/home/pages/index.tsx
+++ src/home/pages/index.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {stylesHome} from './styles';
 
+interface Task {
+  id: string;
+  title: string;
+}
+
 export const Home = () => {
+  const [newTask, setNewTask] = useState('');
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  const handleAddNewTask = () => {
+    const data = {
+      id: new Date().getTime().toString(),
+      title: newTask ? newTask : 'Task empty',
+    };
+
+    setTaskList([...taskList, data]);
+  };
+
   return (
     <SafeAreaView style={stylesHome.safeArea}>
       <View style={stylesHome.container}>
-        <Text style={stylesHome.title}>Oá dev</Text>
+        <Text style={stylesHome.title}>Oá dev!</Text>
+        <TextInput
+          onChangeText={setNewTask}
+          placeholderTextColor="#555"
+          placeholder="Nova Tarefa ..."
+          style={stylesHome.input}
+        />
+        <TouchableOpacity
+          style={stylesHome.button}
+          activeOpacity={0.7}
+          onPress={handleAddNewTask}>
+          <Text style={stylesHome.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+        <Text style={stylesHome.titleTasks}>Minhas Tarefas</Text>
+        {taskList.map(task => (
+          <TouchableOpacity style={stylesHome.buttonTask} key={task.id}>
+            <Text style={stylesHome.taskTextContent}>{task.title}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </SafeAreaView>
   );
--- src/home/pages/styles.ts
+++ src/home/pages/styles.ts
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 
 export const stylesHome = StyleSheet.create({
   safeArea: {
@@ -14,6 +14,46 @@ export const stylesHome = StyleSheet.create({
   title: {
     color: '#f1f1f1',
     fontSize: 24,
+    fontWeight: 'bold',
+  },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  input: {
+    color: '#fff',
+    fontSize: 20,
+    backgroundColor: '#29292e',
+    padding: Platform.OS === 'ios' ? 15 : 12,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  button: {
+    fontSize: 20,
+    backgroundColor: '#eba417',
+    padding: Platform.OS === 'ios' ? 15 : 12,
+    marginTop: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonTask: {
+    backgroundColor: '#29292e',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  taskTextContent: {
+    color: '#f1f1f1',
+    fontSize: 20,
     fontWeight: 'bold',
   },
 });
