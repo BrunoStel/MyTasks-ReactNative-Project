6b788178b560f12e4f08c7297a8a4300baefca06
Bruno <brunostel@gmail.com>
2022-02-10 09:41:23 -0300

    test: ensure home page inserts an item in the task list

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,2 @@
-export default '@react-native-async-storage/async-storage/jest/async-storage-mock';
+export default from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
--- __tests__/pages/home.spec.tsx
+++ __tests__/pages/home.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import {Home} from '../../src/home/pages';
+import {renderHook, act} from '@testing-library/react-hooks';
+import {TaskProvider, useTaskList} from '../../src/context/TasksContex';
 
 describe('Home page', () => {
   it('Should renders text input correctly', () => {
@@ -10,5 +12,20 @@ describe('Home page', () => {
 
     expect(inputNewTask).toBeDefined();
     expect(inputNewTask.props.placeholder).toBeTruthy();
+  });
+
+  it('Sould insert a item in the task list ', async () => {
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TaskProvider,
+    });
+
+    const data = {
+      id: 'any_id',
+      title: 'any_title',
+    };
+
+    await act(() => result.current.addTask(data));
+
+    expect(result.current.tasks).toBeTruthy();
   });
 });
